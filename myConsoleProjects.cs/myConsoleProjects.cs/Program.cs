44ef000b5c6150e82e263c9a919215e6ae876af7
MikeLouns <michael@cloudcade.com>
2021-11-29 15:45:45 -0800

    updated assignment

--- myConsoleProjects.cs/myConsoleProjects.cs/Program.cs
+++ myConsoleProjects.cs/myConsoleProjects.cs/Program.cs
@@ -15,21 +15,24 @@ namespace myConsoleProjects.cs
             string course = Console.ReadLine();
             Console.WriteLine("What page are you on?");
             string page = Console.ReadLine();
+            int pageint = Convert.ToInt32(page);
             Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False.\"");
             string help = Console.ReadLine();
+            bool helpbool = Convert.ToBoolean(help);
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
             string experience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
             string hours = Console.ReadLine();
+            int hoursint = Convert.ToInt32(hours);
             Console.WriteLine(name);
             Console.WriteLine(course);
-            Console.WriteLine(page);
-            Console.WriteLine(help);
+            Console.WriteLine(pageint);
+            Console.WriteLine(helpbool);
             Console.WriteLine(experience);
             Console.WriteLine(feedback);
-            Console.WriteLine(hours);
+            Console.WriteLine(hoursint);
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
---
+++ myConsoleProjects.cs/myConsoleProjects.cs/product.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace myConsoleProjects.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 5;
+            int num2 = 7
+            int product = num1 * num2;
+            Console.WriteLine(product);
+        }
+    }
+}
