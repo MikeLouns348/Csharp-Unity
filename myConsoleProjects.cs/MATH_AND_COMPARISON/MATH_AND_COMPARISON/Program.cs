65fc7427e0d0592ea3d9bb710feaf82fe6e522e2
MikeLouns <michael@cloudcade.com>
2021-12-02 12:30:53 -0800

    Income Comparison Assignment

---
+++ myConsoleProjects.cs/MATH_AND_COMPARISON/MATH_AND_COMPARISON/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace myConsoleProjects.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is your hourly rate?");
+            string input1 = Console.ReadLine();
+            int input1Int = Convert.ToInt32(input1);
+            
+            Console.WriteLine("How many hours do you work per week?");
+            string input2 = Console.ReadLine();
+            int input2Int = Convert.ToInt32(input2);
+
+            double salaryPerWeek1 = input1Int * input2Int;
+            double salaryPerMonth1 = salaryPerWeek1 * 4;
+            double salaryPerYear1 = salaryPerMonth1 * 12;
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is your hourly rate?");
+            string input3 = Console.ReadLine();
+            int input3Int = Convert.ToInt32(input3);
+
+            Console.WriteLine("How many hours do you work per week?");
+            string input4 = Console.ReadLine();
+            int input4Int = Convert.ToInt32(input4);
+
+            double salaryPerWeek2 = input3Int * input4Int;
+            double salaryPerMonth2 = salaryPerWeek2 * 4;
+            double salaryPerYear2 = salaryPerMonth2 * 12;
+
+            Console.WriteLine("Annual Salary of Person 1:");
+            Console.WriteLine(salaryPerYear1);
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            Console.WriteLine(salaryPerYear2);
+
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            bool salaryComp = salaryPerYear1 > salaryPerYear2;
+            Console.WriteLine(salaryComp);
+
+            Console.Read();
+        }
+    }
+}
---
+++ myConsoleProjects.cs/step125Assignment/step125Assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace myConsoleProjects.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int var1 = 50;
+            int var2 = 25;
+            double var3 = 12.5;
+            int var4 = 7;
+
+            Console.WriteLine("Choose a number to multiply by 50.");
+            string input1 = Console.ReadLine();
+            int input1Int = Convert.ToInt32(input1);
+            int solution1 = input1Int * var1;
+            Console.WriteLine(solution1);
+            
+
+            Console.WriteLine("Choose a number to add 25 to.");
+            string input2 = Console.ReadLine();
+            int input2Int = Convert.ToInt32(input2);
+            int solution2 = input2Int + var2;
+            Console.WriteLine(solution2);
+
+            Console.WriteLine("Choose a number to divide by 12.5.");
+            string input3 = Console.ReadLine();
+            int input3Int = Convert.ToInt32(input3);
+            double solution3 = input3Int / var3;
+            Console.WriteLine(solution3);
+
+            Console.WriteLine("Choose a number to check if its greater than 50.");
+            string input4 = Console.ReadLine();
+            int input4Int = Convert.ToInt32(input4);
+            bool solution4 = input4Int > var1;
+            Console.WriteLine(solution4);
+
+            Console.WriteLine("Choose a number to divide by 7 and see the remainder.");
+            string input5 = Console.ReadLine();
+            int input5Int = Convert.ToInt32(input5);
+            double solution5 = input5Int % var4;
+            Console.WriteLine(solution5);
+
+            Console.Read();
+        }
+    }
+}
