9520eb8d995d2653e7d3621bb6769ac40ac113cc
MikeLouns <michael@cloudcade.com>
2021-12-02 16:41:26 -0800

    Boolean Logic Assignment

---
+++ myConsoleProjects.cs/BoolLogic/BoolLogic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace myConsoleProjects.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string input1 = Console.ReadLine();
+            int input1Int = Convert.ToInt32(input1);
+
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            string input2 = Console.ReadLine();
+            bool input2Int = Convert.ToBoolean(input2);
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            string input3 = Console.ReadLine();
+            int input3Int = Convert.ToInt32(input3);
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (input1Int > 15 && input2Int == false && input3Int <= 3);
+            Console.WriteLine(isQualified);
+            
+            Console.Read();
+        }
+    }
+}
+
