ad9ab46ba47cdb6e56dba9522eef5299361b8858
MikeLouns <michael@cloudcade.com>
2021-12-08 10:56:56 -0800

    array Assignment

---
+++ myConsoleProjects.cs/ArrayAssignment/ArrayAssignment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        string[] jediArray = { "Yoda", "Mace Windu", "Qui-Gon Jinn", "Obi-Wan Kenobi", "Ashoka Tano" };
+        
+        Console.WriteLine("Which Jedi would you be.");
+        Console.WriteLine("Enter a value between 1 and 5");
+        int input1 = Convert.ToInt32(Console.ReadLine());
+
+        if (input1 > 5 || input1 < 0) 
+        {
+            Console.WriteLine("FAIL");
+            Console.ReadLine();
+        }
+        else
+        {
+            int choice = input1 - 1;
+            Console.WriteLine(jediArray[choice]);
+            Console.ReadLine();
+        }
+
+        int[] numArray = { 0, 0, 0, 50000000, 0 };
+        Console.WriteLine("Pick a number between 1 and 5 to find out how much you've won!");
+        
+        int input2 = Convert.ToInt32(Console.ReadLine());
+
+        if (input2 > 5 || input2 < 0)
+        {
+            Console.WriteLine("FAIL");
+            Console.ReadLine();
+        }
+        else
+        {
+            int choice = input2 - 1;
+            Console.WriteLine(numArray[choice]);
+            Console.ReadLine();
+        }
+
+        List<string> jediList = new List<string>();
+        jediList.Add("Yoda");
+        jediList.Add("Mace Windu");
+        jediList.Add("Qui-Gon Jinn");
+        jediList.Add("Obi-Wan Kenobi");
+        jediList.Add("Ashoka Tano");
+
+        Console.WriteLine("Which Jedi would you be.");
+        Console.WriteLine("Enter a value between 1 and 5");
+        int input3 = Convert.ToInt32(Console.ReadLine());
+
+        if (input3 > 5 || input3 < 0)
+        {
+            Console.WriteLine("FAIL");
+            Console.ReadLine();
+        }
+        else
+        {
+            int choice = input3 - 1;
+            Console.WriteLine(jediList[choice]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myConsoleProjects.cs/Arrays/Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //ARRAYS
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+        //numArray2[3] = 200000;
+
+        //Console.WriteLine(numArray2[3]);
+        //Console.ReadLine();
+
+        //LISTS
+
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        //intList.Remove(10);
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("Jesse");
+        intList.Remove("James");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+    }
+}
+
