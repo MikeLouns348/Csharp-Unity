0bf306600e4fde3b8f7cda035ecbed6dc7745d0d
MikeLouns <michael@cloudcade.com>
2021-12-09 10:16:09 -0800

    exception handling

---
+++ myConsoleProjects.cs/ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<int> numbers = new List<int>() { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+            Console.WriteLine("Pick a number to divide each number in the list by.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in numbers)
+            {
+                int result = number / input1;
+                Console.WriteLine(result);
+            }            
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("You can't divide by 0.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("You've exited the try catch block");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myConsoleProjects.cs/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            
+        }
+    
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("You can't divide by 0.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        finally
+        {
+            Console.ReadLine();
+        }
+       
+    }
+}
