b5a64fa8dfadc6f7bccd5594d7a602e8437713d1
MikeLouns <michael@cloudcade.com>
2021-12-13 13:55:35 -0800

    MethodMath4

---
+++ myConsoleProjects.cs/MethodMath4/MethodMath4/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath4
+{
+    class Math3
+    {
+        public void Math(int num1, int num2)
+        {
+
+            Console.WriteLine(num1 * 5);
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ myConsoleProjects.cs/MethodMath4/MethodMath4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math3 math = new Math3();
+            Console.WriteLine("Type a number");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type a second number.");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            math.Math(userNum1, userNum2);
+            Console.ReadLine();
+        }
+        
+    }
+}
