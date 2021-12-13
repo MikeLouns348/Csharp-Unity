0fc05ec170ea9b092e5e4be38c2fafa89b8be399
MikeLouns <michael@cloudcade.com>
2021-12-13 13:38:31 -0800

    MethodMath3

---
+++ myConsoleProjects.cs/MethodMath3/MethodMath3/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath2
+{
+    class Math2
+    {
+        public void Math(int num1, int num2 = 1)
+        {
+   
+            Console.WriteLine(num1 * num2 * 5);
+        }
+
+       
+
+    }
+}
---
+++ myConsoleProjects.cs/MethodMath3/MethodMath3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math2 math = new Math2();
+            Console.WriteLine("Type a number");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("If you like, you may type a second number, or not.");
+                int userNum2 = Convert.ToInt32(Console.ReadLine());
+                math.Math(userNum1, userNum2);
+
+            }
+            catch
+            {
+                math.Math(userNum1);
+            }
+            
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
