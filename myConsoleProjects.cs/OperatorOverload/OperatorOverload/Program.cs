f48089247efa616c86048141a9ee5019c2d40efd
MikeLouns <michael@cloudcade.com>
2021-12-15 12:38:13 -0800

    operator overload

---
+++ myConsoleProjects.cs/OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.Id = 1111;
+            employee.SayName();
+            Console.WriteLine("Employee ID: " + employee.Id); ;
+
+            Employee employee2 = new Employee();
+            employee.FirstName = "Student";
+            employee.LastName = "Sample";
+            employee.Id = 2222;
+            employee.SayName();
+            Console.WriteLine("Employee ID: " + employee.Id); ;
+
+            Console.WriteLine(employee.Id == employee2.Id);
+            Console.WriteLine(employee.Id != employee2.Id);
+
+            //employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
--- myConsoleProjects.cs/TwentyOne/TwentyOne/Program.cs
+++ myConsoleProjects.cs/TwentyOne/TwentyOne/Program.cs
@@ -24,25 +24,33 @@ namespace TwentyOne
             //example of polymorphism below 
             //Game game = new TwentyOneGame();
 
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "mike", "hanna" , "ethan"};
-            game.ListPlayers();
-            Console.ReadLine();
-
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-
-            //foreach (Card card in deck.Cards)
-            //{
-
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-
-
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "mike", "hanna" , "ethan"};
+            //game.ListPlayers();
             //Console.ReadLine();
+
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Mike";
+            //game = game + player;
+            //game = game - player;
+            
+            //adds the palyer
+            game += player;
+            //removes the player
+            game -= player;
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
         
