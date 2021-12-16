7c7283057ff316104cb1e0a2da3945236531c6dc
MikeLouns <michael@cloudcade.com>
2021-12-15 16:27:26 -0800

    generic parameter

---
+++ myConsoleProjects.cs/GenericParameter/GenericParameter/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameter
+{
+    class Employee<T> : Person
+    {
+        public Employee()
+        {
+            Id = 007;
+            
+        }
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+    }
+}
---
+++ myConsoleProjects.cs/GenericParameter/GenericParameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>() { "apron", "pen" };
+            
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.things = new List<int>() { 1, 2, 3 };
+
+            foreach (string thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+            foreach(int thing in employee2.things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
